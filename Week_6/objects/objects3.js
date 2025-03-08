a5c891942868c8f16b56961dc9b3cfd35885efaf
Huttunenbe <beniittahuttunen@gmail.com>
2025-03-08 10:32:59 +0200

    add comments

--- Week_6/objects/objects3.js
+++ Week_6/objects/objects3.js
@@ -172,7 +172,7 @@ const weatherApp = {
     fetch(url)
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok " + response.statusText);
+          throw new Error("Network response not ok " + response.statusText);//incase fails
         }
         return response.json();
       })
@@ -189,8 +189,8 @@ const weatherApp = {
           humidity: humidity,
         });
       })
-      .catch((error) => console.error("Fetching weather data failed:", error));
-  },
+      .catch((error) => console.error("Fetching data failed:", error));
+  },//incase fails
 };
 
 function displayWeather(data) {
