eea873ba931d708b8f24eb3698f4de51aa1e0a4d
Huttunenbe <beniittahuttunen@gmail.com>
2025-03-14 18:50:25 +0200

    small fixes

--- Week_4/basic_DOM_5/script.js
+++ Week_4/basic_DOM_5/script.js
@@ -2,7 +2,9 @@ const button = document.getElementById("button");
 const paragraph = document.getElementById("colorChanger");
 const input = document.getElementById("colorInput");
 
-input.addEventListener("input", () => (paragraph.style.color = input.value));
+input.addEventListener("input", () => {
+  paragraph.style.color = input.value;
+});
 
 function random(max) {
   return Math.floor(Math.random() * max);
